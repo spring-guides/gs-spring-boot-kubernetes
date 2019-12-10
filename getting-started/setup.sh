d5ed7b05abcdabe3cc843cdc1ec0728ae1c0d550
Dave Syer <dsyer@pivotal.io>
2019-12-10 13:51:49 +0000

    Registry works without modifying daemon.json

--- getting-started/setup.sh
+++ getting-started/setup.sh
@@ -1,3 +1,1 @@
 docker run -d -p 80:5000 --name registry registry:2
-daemonConfig='/etc/docker/daemon.json'
-jq -s '.[0] * .[1]' <( cat ${daemonConfig}) <(echo '{ "insecure-registries": [ "localhost" ] }') | sudo tee ${daemonConfig} > /dev/null
