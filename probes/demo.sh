0550b2da7de0ec5e126981e0fd361fbfd5034a75
Greg L. Turnquist <gturnquist@vmware.com>
2021-04-26 18:37:51 -0500

    Migrate from master to main.

--- probes/demo.sh
+++ probes/demo.sh
@@ -1,4 +1,4 @@
 mkdir demo
-(cd demo; curl -L https://github.com/spring-guides/gs-spring-boot-kubernetes/archive/master.tar.gz | tar xz gs-spring-boot-kubernetes-master/initial --strip-components 2)
+(cd demo; curl -L https://github.com/spring-guides/gs-spring-boot-kubernetes/archive/main.tar.gz | tar xz gs-spring-boot-kubernetes-main/initial --strip-components 2)
 
 docker run -d -p 80:5000 --name registry registry:2
